d1c9c267f12e28cc5c4a1113a88dc19dd7f57207
ChunHan08 <149086722+ChunHan08@users.noreply.github.com>
2024-07-14 23:58:06 +0000

    final

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const character = document.querySelector(".character");
-const obstacle = document.querySelector(".obstacles");
+const obstacles = document.querySelector(".obstacles");
 const scoreText = document.querySelector(".score");
 let score = 0;
 let isStarted = false;
@@ -12,4 +12,42 @@ function jump() {
       character.classList.remove("jump");
     }, 700);
   }
+}
+
+function startMoving() {
+  if (obstacles.classList !== "move") {
+    obstacles.classList.add("move")
+  }
+}
+
+function start() {
+  if (!isStarted) {
+    isStarted = true;
+    startMoving();
+    checkDead();
+    scoreCounter()
+    
+  }
+}
+
+function scoreCounter() {
+  if (!isStarted) {
+    return;
+  }
+  score++;
+  scoreText.innerHTML = "Score: " + Math.round(score / 20)
+  requestAnimationFrame(scoreCounter)
+}
+function checkDead() {
+  let characterTopPosition = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
+
+  let obstacleLeftPosition = parseInt(window.getComputedStyle(obstacles).getPropertyValue("left"));
+
+  if (obstacleLeftPosition <= 80 && obstacleLeftPosition >= 10 && characterTopPosition >= 60) {
+    isStarted = false;
+    obstacles.classList.remove("move");
+    alert("Game Over!")
+    score = 0;
+  }
+  requestAnimationFrame(checkDead)
 }
